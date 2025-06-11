01b8eeace9bc2e699f9be31bfb5994a637cc2166
anatrko <anacarolinaturko@gmail.com>
2025-06-11 10:36:35 -0300

    continuação

--- eventos/urls.py
+++ eventos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lista_eventos, name='lista_eventos'),
+    path('lista/', views.lista_eventos, name='lista_eventos'),
     path('criar/', views.criar_evento, name='criar_evento'),
     path('editar/<int:id>/', views.editar_evento, name='editar_evento'),
     path('excluir/<int:id>/', views.excluir_evento, name='excluir_evento'),
